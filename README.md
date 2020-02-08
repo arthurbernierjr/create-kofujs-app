3ba01436d2f6abb8ca04cd34b5e3bd8b590c753c
arthurbernierjr <ma2bernier@gmail.com>
2020-02-08 06:12:48 -0500

    mad changes for dev

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
 1. clone the repository
 2. add any template that you want to the templates directory
 3. run ``` npm i create-kofujs-app -g ``` in the root of the project and it will add the project globally onto your computer
-4. from the command line run ``` generate ``` in the parent folder that you desire the project to be in.
-5. Feel awesome
+4. from the command line run ``` generate-kofujs ``` in the parent folder that you desire the project to be in.
+5. You will select kofujs which is the only current template more to come soon
+6. It will ask your project name that will be the name of your folder
+7. Feel awesome
+8. Write your components in the src folder, don't delete templates
+NOTE KOFU JS STARTER COMMANDS ONLY WORK WITH YARN use NPM if you want and make prs for fixes if you desire.
--- templates/kofujs/src/example/index.js
+++ templates/kofujs/src/example/index.js
@@ -1,113 +1,28 @@
 import {
   Komponent,
   render
-} from "../../lib/kofu-framework/index.js";
+} from "../../framework/lib/kofu-framework/index.js";
 
-export class HelloWorld extends Komponent {
-  constructor(props) {
-    super(props);
+class Example extends Komponent{
+  constructor (props)  {
+    super (props)
     const styles = {
-      mainHeader : {
-        backgroundImage: `url('/assets/img/bg3.jpg')`
+      mainPageHeading: {
+        backgroundColor: 'red'
       }
     }
-    // Adds the created CSS Classes to this
-    const { classes } = this.setStyles(styles)
-    this.classes = classes
-    // User this.state or this.data or both
-    this.state = {
-      reactive: " this data must be updated with SetState"
-    }
-    this.data = {
-      unReactive: "this is available also and this will be just an object and previous data is unreachable after a key is overwritten"
-    }
-    // Bind Functions if necessary
-    this.handleClick = this.handleClick.bind(this)
+    this.classes = this.setStyles(styles).classes
   }
-  komponentGenesis(){
-    console.log('genesis')
-  }
+
   komponentDidMount () {
-    console.log('mounted')
+    console.log('Hello world')
   }
-  handleClick() {
-    console.log('hi')
-    alert('On Click Works')
-  }
-// This is where you put the jsx that you want to render or present it is called present so that it doesn't get confused by the render method outsided the class
-  present() {
-    console.log('rendering')
+
+  present () {
     return (
-      <div>
-        <nav className="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg" color-on-scroll="100">
-          <div className="container">
-            <div className="navbar-translate">
-              <a className="navbar-brand" href="#">
-              KofuJS </a>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation"
-              onClick={this.handleClick}
-              >
-                <span className="sr-only">Toggle navigation</span>
-                <span className="navbar-toggler-icon"></span>
-                <span className="navbar-toggler-icon"></span>
-                <span className="navbar-toggler-icon"></span>
-              </button>
-          </div>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <i className="material-icons">apps</i> Template
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <div
-        className={`page-header header-filter ${this.classes.mainHeader}`}
-        data-parallax="true"
-        >
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 ml-auto mr-auto">
-              <div className="brand text-center">
-                <h1>Hello World</h1>
-                <h3 className="title text-center">It works</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="main main-raised">
-        <div className="container">
-          <div className="section text-center">
-            <h2 className="title">KofuJS Concept Page</h2>
-          </div>
-        </div>
-      </div>
-      <footer className="footer footer-default">
-        <div className="container">
-          <nav className="float-left">
-            <ul>
-              <li>
-                <a href="https://kofujs.io/">
-                  Built with KofuJS
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="copyright float-right">
-            &copy;
-              { new Date().getFullYear() }, made with <i className="material-icons">favorite</i> by{' '}
-            <a href="https://arthurbernierjr.com/" target="blank">Arthur Bernier Jr.</a> for a better web.
-          </div>
-        </div>
-        </footer>
-      </div>
+        <h1 className={`${this.classes.mainPageHeading}`}> Hello World</h1>
     )
   }
+}
 
-};
-
-render(<HelloWorld />, document.getElementById("app"));
+render(<Example />, document.getElementById('app'))
