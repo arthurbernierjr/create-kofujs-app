8435bde6eeac40986fcbdb0ec80143f4aa5442e2
Arthur <ma2bernier@gmail.com>
2020-03-27 13:49:54 -0400

    Update to Alpha 5

--- templates/kofu-starter-app/webpack.config.js
+++ templates/kofu-starter-app/webpack.config.js
@@ -3,9 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// Start with set up here and then set up a version to work with this as a node module
 
 module.exports = env => {
   return {
